ff3bc65adf7c6a65ad57ed6e74f21629ecadbd4c
DrShahidsharif <hdr.mshahid24@gmail.com>
2025-02-08 00:02:45 +0500

    Updated postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,9 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // Ensure this is enabled for Next.js 13+
   },
 };
 
-export default config;
+export default nextConfig;
